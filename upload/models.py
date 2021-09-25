948b0035319c11a2c150104e419065ef1c3df3aa
Ishant Dahiya <ishantdahiya@gmail.com>
2021-09-25 18:16:51 +0530

    basic file config

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class FileItem(models.Model):
+    name = models.CharField(max_length=120, null=True, blank=True)
+    path = models.TextField(blank=True, null=True)
+    size = models.BigIntegerField(default=0)
+    file = models.FileField(null=True, blank=True)
+    file_type = models.CharField(max_length=120, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    uploaded = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    processed = models.BooleanField(default=False)
+
+    @property
+    def title(self):
+        return str(self.name)
