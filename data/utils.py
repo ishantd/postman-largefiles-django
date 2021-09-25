a7d68747c8c7ea59058362f26da152f9eab20abe
Ishant Dahiya <ishantdahiya@gmail.com>
2021-09-26 01:15:35 +0530

    product aggregate process done

--- data/utils.py
+++ data/utils.py
@@ -1,8 +1,9 @@
-from data.models import Product
+from data.models import Product, ProductAggregate
 from upload.models import FileItem
 from upload.utils import random_string_generator
 
 from django.conf import settings
+from django.db.models import Count, aggregates
 
 from tqdm import tqdm
 import pandas as pd
@@ -24,18 +25,28 @@ class ProcessCSV:
             products.append(
                 Product(
                     name=row[1],
-                    sku=row[2] if index==0 else f'{row[2]}-{random_string_generator(8)}',
+                    sku=f'{row[2]}-{random_string_generator(8)}',
                     description=row[3]
                 )
             )
         products = Product.objects.bulk_create(objs=products)
         file.processed = True
         file.save()
+        return products
+    
+    def aggregate_data_into_table(self, products):
+        fieldname = 'name'
+        product_count = list(Product.objects.all().values(fieldname).order_by(fieldname).annotate(product_count=Count(fieldname)))
+        product_count_objects = [ProductAggregate(name=p["name"], product_count=p["product_count"]) for p in product_count]
+        ProductAggregate.objects.all().delete()
+        new_product_aggregate = ProductAggregate.objects.bulk_create(objs=product_count_objects)
+        return new_product_aggregate
     
     def start_file_processing(self):
         if len(self.processed_files) == 0 and len(self.files) == 1:
             df = self.create_pandas_object(self.files[0])
             saved_to_db = self.save_df_into_db(df, self.files[0], 0)
+            aggregated_data = self.aggregate_data_into_table(saved_to_db)
         return True
         
 
