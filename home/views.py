156c0e851baf5264122b0bb484ee4a4f8bbd460a
Ishant Dahiya <ishantdahiya@gmail.com>
2021-09-26 03:26:59 +0530

    some quick stats

--- home/views.py
+++ home/views.py
@@ -1,13 +1,20 @@
 from django.core import paginator
 from django.core.paginator import Paginator
+from django.db.models import aggregates
 from django.shortcuts import render
 
-from data.models import Product
+from data.models import Product, ProductAggregate
 
 def index(request):
-    products_list = Product.objects.all()
+    products_list = Product.objects.all().order_by('name')
+    aggregate_list = ProductAggregate.objects.all().order_by('-product_count')[:15].values('name', 'product_count')
     paginator = Paginator(products_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj}
+    context = {
+        'page_obj': page_obj,
+        'product_count' : Product.objects.all().count(),
+        'aggregate_table_count' : ProductAggregate.objects.all().count(),
+        'aggregate_list': aggregate_list
+    }
     return render(request, 'home/index.html', context)
