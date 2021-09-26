6363c48ca9947cd8fce3b16a281704105221d1f6
Ishant Dahiya <ishantdahiya@gmail.com>
2021-09-26 21:55:07 +0530

    some more changes

--- data/views.py
+++ data/views.py
@@ -4,13 +4,17 @@ from django.forms import model_to_dict
 from rest_framework.views import APIView
 
 from data.models import Product, ProductAggregate
+from data.utils import ProcessCSV, start_thread_process
 from braces.views import CsrfExemptMixin
 
 class Products(CsrfExemptMixin, APIView):
     
     def get(self, request, *args, **kwargs):
         query = request.query_params.get('sku', False)
-        product = Product.objects.get(sku=query)
+        try:
+            product = Product.objects.get(sku=query)
+        except:
+            return JsonResponse({"status": "ok", "product": False}, status=200)
         return JsonResponse({"status": "ok", "product": model_to_dict(product)}, status=200)
     
     def post(self, request, *args, **kwargs):
@@ -19,6 +23,9 @@ class Products(CsrfExemptMixin, APIView):
         description = request.data.get('product_description', False)
         
         product = Product.objects.get(sku=product_sku)
+        if not(product.name == name):
+            p = ProcessCSV()
+            start_thread_process(p.aggregate_data_into_table())
         product.name = name
         product.description = description
         product.save()
