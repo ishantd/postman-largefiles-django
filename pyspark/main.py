eae70dcc71480d5486e7e2f53fb0f54145be63ab
Ishant Dahiya <ishantdahiya@gmail.com>
2021-09-28 03:20:43 +0530

    class based pyspark functionality

--- pyspark/main.py
+++ pyspark/main.py
@@ -2,39 +2,46 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 import os
+import sys
 import uuid
 import findspark
-from tqdm import tqdm
 findspark.add_packages('org.postgresql:postgresql:42.2.24')
 
-from io import StringIO
-import pandas as pd
-import psycopg2
 
-spark = SparkSession.builder.appName('test_new') \
-.config('spark.driver.memory','10G') \
-.config('jars', '/opt/application/postgresql-42.2.24.jar') \
-.config('spark.driver.extraClassPath', 'postgresql-42.2.24.jar') \
-.getOrCreate()
+class PySparkProcess:
+    
+    def __init__(self):
+        self.spark = SparkSession.builder.appName('test_new') \
+        .config('spark.driver.memory','10G') \
+        .config('jars', '/opt/application/postgresql-42.2.24.jar') \
+        .config('spark.driver.extraClassPath', 'postgresql-42.2.24.jar') \
+        .getOrCreate()
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--driver-class-path /opt/application/postgresql-42.2.24.jar --jars /opt/application/postgresql-42.2.24.jar pyspark-shell'
+        os.environ['PYSPARK_SUBMIT_ARGS'] = '--driver-class-path /opt/application/postgresql-42.2.24.jar --jars /opt/application/postgresql-42.2.24.jar pyspark-shell'
 
-db_properties={}
-db_url = f'jdbc:postgresql://pm-test.cutudajvwdwp.ap-south-1.rds.amazonaws.com:5432/postgres?user=postgres&password=ACXIWFNY4i688J0HfyPo'
-db_properties['username'] = "postgres"
-db_properties['password'] = "ACXIWFNY4i688J0HfyPo"
-db_properties['url'] = db_url
-db_properties['driver'] = 'org.postgresql.Driver'
+        self.db_properties={}
+        self.db_url = f'jdbc:postgresql://pm-test.cutudajvwdwp.ap-south-1.rds.amazonaws.com:5432/postgres?user=postgres&password=ACXIWFNY4i688J0HfyPo'
+        self.db_properties['username'] = "postgres"
+        self.db_properties['password'] = "ACXIWFNY4i688J0HfyPo"
+        self.db_properties['url'] = self.db_url
+        self.db_properties['driver'] = 'org.postgresql.Driver'
 
-random_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
-df_pyspark = spark.read.csv('products.csv', header=True, inferSchema=True).na.drop('any').drop('sku')
-df_pyspark = df_pyspark.withColumn('sku', random_udf())
-agg_df_pyspark = df_pyspark.groupBy('name').count()
-df_pyspark.show()
-agg_df_pyspark.show()
+    def read_csv(self):
+        random_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
+        self.df_pyspark = self.spark.read.csv('products.csv', header=True, inferSchema=True).na.drop('any').drop('sku')
+        self.df_pyspark = self.df_pyspark.withColumn('sku', random_udf())
+        
+    def write_products_to_db(self):
+        self.df_pyspark.write.jdbc(url=self.db_url, table="data_product", mode='append', properties=self.db_properties)
+     
+    def aggregate_and_write_products_to_db(self):
+        self.producuts_df = self.spark.read \
+        .format("jdbc") \
+        .option("url", self.db_url) \
+        .option("dbtable", 'data_product') \
+        .option("user", "postgres") \
+        .option("password", "ACXIWFNY4i688J0HfyPo") \
+        .load()
+        self.agg_df_pyspark = self.producuts_df.groupBy('name').count()
+        self.agg_df_pyspark.write.jdbc(url=self.db_url, table="data_productaggregate", mode='overwrite', properties=self.db_properties)
 
-# df_pyspark.write.jdbc(url=db_url, table="data_product", mode='overwrite', properties=db_properties)
-# agg_df_pyspark.write.jdbc(url=db_url, table="data_product", mode='overwrite', properties=db_properties)
-# print("SAVING DATA")
-# pd_df = df_pyspark.toPandas()
-# print(pd_df)
